a710f83f1dd66e6a53a2c33db8500d97e3af540e
jaime-olucha <jaime.olucha@cuatroochenta.com>
2026-04-29 09:48:37 +0200

    feat: finish base dashboard with weekly imputed hours

--- src/ui/pages/DashboardPage/DashboardPage.tsx
+++ src/ui/pages/DashboardPage/DashboardPage.tsx
@@ -9,27 +9,75 @@ import { ROUTES } from "@/ui/routes/routes";
 import './DashboardPage.scss';
 import logoWhite from "../../assets/logo-480/480dev_white.webp";
 import { Link } from "react-router-dom";
+import type { TimeEntry } from "@/domain/entities/TimeEntry";
+import { getUserTimeEntriesApi } from "@/infrastructure/api/timeEntriesRepository";
 
 export const DashboardPage = () => {
-  const user = useUserStore((s) => s.user);
+  const user = useUserStore((store) => store.user);
   const [projects, setProjects] = useState<UserProjects[]>([]);
+  const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
+  const [totalHours, setTotalHours] = useState<number>(0);
+
 
   useEffect(() => {
     if (!user) return;
 
     getUserProjectsApi(user.id).then(setProjects);
+    getUserTimeEntriesApi(user.id).then(response => {
+      setTimeEntries(response.data)
+      setTotalHours(response.total_hours)
+    });
+
   }, [user])
 
 
   const getInitials = (name: string = "") => {
     return name?.[0] ?? "";
   }
+  const initials = getInitials(user?.name) + getInitials(user?.surname);
 
   const getRoleBadge = () => {
     if (user?.role == SYSTEM_ROLES.ADMIN) return "Administrador"
   }
 
-  const initials = getInitials(user?.name) + getInitials(user?.surname);
+  const getWeeklyHours = (entries: TimeEntry[]) => {
+    const days = ["Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"];
+    const months = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+
+    const week = [];
+
+    const today = new Date();
+    const currentDay = today.getDay();
+
+    const diffToMonday = currentDay === 0 ? 0 : 1 - currentDay;
+
+    const monday = new Date(today);
+    monday.setDate(today.getDate() + diffToMonday);
+
+    for (let i = 0; i < days.length; i++) {
+      const date = new Date(monday);
+      date.setDate(monday.getDate() + i);
+
+      week.push({
+        day: `${days[i]} ${date.getDate()} ${months[date.getMonth()]}`,
+        dateISO: date.toISOString().split("T")[0],
+        total: 0,
+      });
+    }
+
+    for (let i = 0; i < entries.length; i++) {
+      const entry = entries[i];
+
+      for (let j = 0; j < week.length; j++) {
+        if (entry.date === week[j].dateISO) {
+          week[j].total += entry.hour;
+        }
+      }
+    }
+
+    return week;
+  };
+  const weeklyHours = getWeeklyHours(timeEntries);
 
   return (
     <section className="dashboard-page">
@@ -51,7 +99,7 @@ export const DashboardPage = () => {
             <p><User className="iconSvg" /><strong>Name:</strong> {user?.name} {user?.surname}</p>
             <p><Mail className="iconSvg" /><strong>Correo:</strong> {user?.email}</p>
           </div>
-          <span className="card_badge">{getRoleBadge()}</span>
+          {getRoleBadge() && <span className="card_badge">{getRoleBadge()}</span>}
         </article>
 
         <article className="card">
@@ -87,25 +135,25 @@ export const DashboardPage = () => {
         </article>
 
         <article className="card">
-          <div className="projects-header">
+          <div className="hours-header">
             <div>
-              <h2 className="card_header">Horas Imputadas</h2>
-              <p className="info">Total todas las imputaciones: { }</p>
+              <h2 className="card_header">Horas Imputadas esta semana</h2>
+              <p className="info">Total todas las imputaciones: <strong>{totalHours}h </strong></p>
             </div>
           </div>
-          <div className="card_container">
-            {projects.length === 0 ? (
-              <p>No tienes proyectos asignados</p>
-            ) : (
-              <ul className="projects-list">
-                {projects.slice(0, 4).map((project) => (
-                  <li className="li-map" key={project.id}>
-                    <h2 className="card_header">....</h2>
-                    <p className="info">....</p>
-                  </li>
-                ))}
-              </ul>
-            )}
+
+          <div className="weekly-hours">
+            {weeklyHours.map((day) => (
+              <div className="day-column" key={day.day}>
+                <span className="day-hours">
+                  {day.total > 0 ? `${day.total}h` : "0h"}
+                </span>
+
+                <span className="day-label">
+                  {day.day}
+                </span>
+              </div>
+            ))}
           </div>
         </article>
       </div >
