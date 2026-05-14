f3960b443290865b7f6ca323eee8010b08eac5e7
jaime-olucha <jaime.olucha@cuatroochenta.com>
2026-05-14 09:50:55 +0200

    feat: add manage sectors modal

--- src/domain/repositories/SectorRepository.ts
+++ src/domain/repositories/SectorRepository.ts
@@ -1,5 +1,9 @@
 import type { Sector } from "../models/Client/Sector";
+import type { EntityId } from "../value-objects/EntityId";
 
 export interface SectorRepository {
   getAll(): Promise<Sector[]>;
+  create(name: string): Promise<void>;
+  update(id: EntityId, name: string): Promise<void>;
+  delete(id: EntityId): Promise<void>;
 }
