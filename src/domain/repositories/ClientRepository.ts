542ea47b64c9fde5abda5ed2932cf865efbc3441
jaime-olucha <jaime.olucha@cuatroochenta.com>
2026-05-14 09:54:51 +0200

    fix: repository Client, fix put and delete name

--- src/domain/repositories/ClientRepository.ts
+++ src/domain/repositories/ClientRepository.ts
@@ -1,14 +1,15 @@
 import type { EntityId } from "../value-objects/EntityId";
 import type { Client } from "../models/Client/Client";
 import type { ClientProject } from "../models/Client/ClientProject";
-import type { Contact } from "../models/Client/Contact";
 import type { CreateClientRequest } from "../models/Client/CreateClientRequest";
+import type { UpdateClientRequest } from "../models/Client/UpdateClientRequest";
 
 export interface ClientRepository {
   getAll(page: number, limit: number): Promise<Client[]>;
   getById(id: EntityId): Promise<Client>;
   getProjects(id: EntityId): Promise<ClientProject[]>;
-  getContacts(id: EntityId): Promise<Contact[]>;
   createClient(data: CreateClientRequest): Promise<void>;
   patchActive(id: EntityId, isActive: boolean): Promise<void>;
+  putClient(id: EntityId, data: UpdateClientRequest): Promise<void>;
+  deleteClient(id: EntityId): Promise<void>;
 }
